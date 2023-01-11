a0c794b9fc038db0d60e78400c0e0abc66d34100
BboyAkers <austin.akers5@gmail.com>
2023-01-10 22:23:23 -0800

    refactored _mnemoknicToUint8Array function

--- src/index.ts
+++ src/index.ts
@@ -22,10 +22,15 @@ type KeyringOptions = {
   version?: string;
 };
 
+type Mnemonic = {
+  type: string;
+  data: string;
+};
+
 export default class HdKeyring implements HdKeyringOpts {
   type: string;
 
-  _wallets: any[];
+  _wallets: any[]; // TODO: figure out what type this is
 
   constructor() {
     this.type = type;
@@ -35,6 +40,36 @@ export default class HdKeyring implements HdKeyringOpts {
   _stringToUint8Array(mnemonic: string) {
     const indices = mnemonic.split(' ').map((word) => wordlist.indexOf(word));
     return new Uint8Array(new Uint16Array(indices).buffer);
+  }
+
+  _mnemonicToUint8Array(mnemonic: Mnemonic) {
+    const mnemonicData = mnemonic;
+    // when encrypted/decrypted, buffers get cast into js object with a property type set to buffer
+    if (mnemonic?.type === 'Buffer') {
+      mnemonicData.data = mnemonic.data;
+    }
+
+    if (
+      // this block is for backwards compatibility with vaults that were previously stored as buffers, number arrays or plain text strings
+      typeof mnemonicData.data === 'string' ||
+      Buffer.isBuffer(mnemonicData) ||
+      Array.isArray(mnemonicData)
+    ) {
+      let mnemonicAsString = mnemonicData.data;
+      if (Array.isArray(mnemonicData)) {
+        mnemonicAsString = Buffer.from(mnemonicData).toString();
+      } else if (Buffer.isBuffer(mnemonicData)) {
+        mnemonicAsString = mnemonicData.toString();
+      }
+      return this._stringToUint8Array(mnemonicAsString);
+    } else if (
+      mnemonicData instanceof Object &&
+      !(mnemonicData instanceof Uint8Array)
+    ) {
+      // when encrypted/decrypted the Uint8Array becomes a js object we need to cast back to a Uint8Array
+      return Uint8Array.from(Object.values(mnemonicData.data));
+    }
+    return mnemonicData;
   }
 
   _getWalletForAccount(account: string, opts: KeyringOptions = {}) {
